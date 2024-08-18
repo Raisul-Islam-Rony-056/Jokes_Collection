bdae2f4301111fab471aef992ab12e4a2e8c8db7
Raisul <raisul.islam.rony056@gmail.com>
2024-08-18 17:37:05 +0600

    all

---
+++ BulkyWeb/Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
